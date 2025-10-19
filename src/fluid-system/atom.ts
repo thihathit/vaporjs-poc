d1e6834c0d99a15eb63ff7846d7976f103e844f7
Thiha <git@thiha.dev>
2025-10-20 06:19:47 +0700

    add simple atom

---
+++ src/fluid-system/atom.ts
@@ -0,0 +1,43 @@
+const atomIdentity = Symbol("Atom");
+
+export const createAtom = <T>(initial: T) => {
+  let value = initial;
+
+  type Subscriber = (newValue: T, oldValue: T) => void;
+  type Setter = (current: T) => T;
+
+  const subs = new Set<Subscriber>();
+
+  const get = () => value;
+  const set = (setter: Setter) => {
+    const oldValue = value;
+    const newValue = setter(value);
+
+    value = newValue;
+
+    for (const sub of subs) sub(newValue, oldValue);
+
+    return newValue;
+  };
+
+  const subscribe = (fn: Subscriber) => {
+    subs.add(fn);
+    return () => subs.delete(fn);
+  };
+
+  const atom = { get, set, atomIdentity, subscribe };
+
+  return [atom, set] as const;
+};
+
+export type Atomic<T> = ReturnType<typeof createAtom<T>>;
+
+export type Atom<T> = Atomic<T>[0];
+
+export const isAtom = <T>(value: any): value is Atom<T> => {
+  if (!value) return false;
+
+  return value?.atomIdentity == atomIdentity;
+};
+
+export const toValue = <T>({ get }: Atom<T>): T => get();
