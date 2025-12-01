67dbcfc61a0935c3debbf9a824573b4a4803a3ac
Thiha <git@thiha.dev>
2025-12-01 07:00:47 +0700

    refactor: usage

--- src/main.tsx
+++ src/main.tsx
@@ -1,71 +1,52 @@
-import { connectHoles } from "./web/flusher";
-import { createEffect, createMemo, createSignal } from "./signal";
 import { Index, jsx } from "./web";
-import { shallowEqual, useWorker } from "./utilities";
+import { connectHoles } from "./web/flusher";
 
-const [input, setInput] = createSignal("A");
-const [count1, setCount1] = createSignal(0);
-const [count2, setCount2] = createSignal(0);
-const [dList, setDList] = createSignal([0, 2]);
+import { createMemo, createSignal } from "./signal";
 
-const debug = createMemo(() => JSON.stringify(dList(), null, 2));
+import { useWorker } from "./utilities";
 
-const [memoizeTest, setMem] = createSignal(3);
-const doubledM = createMemo(() => {
-  console.log("computed");
-  return memoizeTest() * 2;
-});
+const Counter1 = () => {
+  const [count1, setCount1] = createSignal(0);
 
-const counters = createMemo(() => ({
-  count1: count1(),
-  count2: count2(),
-}));
+  const increase1 = () => {
+    setCount1((prev) => prev + 1);
+  };
 
-const everything = createMemo(
-  () => ({
-    counters: counters().count2,
-    input: input(),
-  }),
-  shallowEqual,
-);
+  useWorker(
+    // Worker thread: send a tick message every 10ms
+    () => setInterval(() => postMessage("tick"), 10),
+    (data) => {
+      if (data === "tick") increase1();
+    },
+  );
 
-// Original usage: memo that returns an array of values
-const list = createMemo(() => [count1(), count1()]);
-
-const increase2 = () => {
-  setCount2((prev) => prev + 1);
-};
-
-useWorker(
-  // Worker thread: send a tick message every 10ms
-  () => setInterval(() => postMessage("tick"), 10),
-  (data) => {
-    if (data === "tick") setCount1((prev) => prev + 1);
-  },
-);
-
-createEffect(() => {
-  console.log(everything());
-  // console.log(`Count 1: ${count1()} | Count 2: ${count2()}`);
-});
-
-// const App = () => <p>Counter 1: {count1}</p>;
-const App = () => (
-  <div>
+  return (
     <div>
       <p>Counter 1: {count1}</p>
     </div>
+  );
+};
 
-    <hr />
+const Counter2 = () => {
+  const [count2, setCount2] = createSignal(0);
 
+  const increase2 = () => {
+    setCount2((prev) => prev + 1);
+  };
+
+  return (
     <div>
       <p>Counter 2: {count2}</p>
 
       <button onClick={increase2}>Increment</button>
     </div>
+  );
+};
 
-    <hr />
+const FormFields = () => {
+  const [input, setInput] = createSignal("A");
 
+  return (
     <div>
       <p>Input: {input}</p>
 
@@ -77,43 +58,71 @@ const App = () => (
         }}
       />
     </div>
+  );
+};
+
+const MemoizeTest = () => {
+  const [memoizeTest, setMem] = createSignal(3);
+  const doubledM = createMemo(() => {
+    console.log("computed");
+    return memoizeTest() * 2;
+  });
+
+  return (
+    <div>
+      <p>
+        memoizeTest: {memoizeTest} | {doubledM}
+      </p>
+      <button onClick={() => setMem(3)}>Three</button>
+      <button onClick={() => setMem(4)}>Four</button>
+    </div>
+  );
+};
+
+const List = () => {
+  const [dList, setDList] = createSignal([0, 2]);
+
+  return (
+    <div>
+      <button onClick={() => setDList((prev) => [...prev, Math.random()])}>
+        Add
+      </button>
+      <button
+        onClick={() => setDList(([, ...rest]) => [Math.random(), ...rest])}
+      >
+        Update First
+      </button>
+
+      <Index each={dList}>
+        {(item, index) => (
+          <p>
+            Liste item {index}: {item}
+          </p>
+        )}
+      </Index>
+    </div>
+  );
+};
+
+const App = () => (
+  <div>
+    <Counter1 />
 
     <hr />
 
-    <p>
-      memoizeTest: {memoizeTest} | {doubledM}
-    </p>
-    <button onClick={() => setMem(3)}>Three</button>
-    <button onClick={() => setMem(4)}>Four</button>
+    <Counter2 />
 
     <hr />
 
-    <Index each={list}>
-      {(item, index) => (
-        <p>
-          Liste item {index}: {item}
-        </p>
-      )}
-    </Index>
+    <FormFields />
 
     <hr />
 
-    <button onClick={() => setDList((prev) => [...prev, Math.random()])}>
-      Add
-    </button>
-    <button onClick={() => setDList(([, ...rest]) => [Math.random(), ...rest])}>
-      Update First
-    </button>
+    <MemoizeTest />
 
-    {/*<pre>{debug}</pre>*/}
+    <hr />
 
-    <Index each={dList}>
-      {(item, index) => (
-        <p>
-          Liste item {index}: {item}
-        </p>
-      )}
-    </Index>
+    <List />
   </div>
 );
 
